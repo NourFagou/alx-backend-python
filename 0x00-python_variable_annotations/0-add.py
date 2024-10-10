fd286ce7dce3269e5d1e2c6a3840c5a5f31bbe07
NourFagou <nourfagou@gmail.com>
2024-10-10 12:10:09 -0700

    mandatory

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add annontaated function: takes two args
+    a: is a float
+    b: is a float
+    return the sum of them
+    """
+    return a + b
